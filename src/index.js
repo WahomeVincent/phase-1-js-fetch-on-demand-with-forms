f3192ce6612293cf8e0e2ac7f70551a17e10b3b6
WahomeVincentMwangiclear <vinwahome@gmail.com>
2023-03-29 22:24:35 +0300

    Able to fetch dsta from a server/API and log on the console

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 const init = () => {
     const inputForm = document.querySelector('form');
     inputForm.addEventListener('submit', (event) => {event.preventDefault()
-    
+    const input = document.querySelector('input#searchByID')
+
+    console.log(input.value)
 })
+
+    fetch('http://localhost:3000/movies')
+    .then(Response => Response.json())
+    .then(data => {
+        console.log(data)
+    } )
+
+    
   
 }
 
